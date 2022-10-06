64511705f6eddcff9fe1f5ab049e952b23e59ce6
diana <volchenko_93@mail.ru>
2022-10-06 14:03:39 +0300

    Готовое дз

---
+++ task41/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+int getCountOfNumbersGreaterThanNumber(int[] incomingArray, int number)
+{
+    int count = 0;
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        if (incomingArray[i] > number)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Введите числа через пробелы: ");
+int[] currentArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int number = 10;
+int countOfNumbers = getCountOfNumbersGreaterThanNumber(currentArray, number);
+Console.WriteLine($"Количество чисел больше {number} = {countOfNumbers}");
---
+++ task43/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double getUserNumber(string name)
+{
+    Console.Write(name + " ");
+    double number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+double[] getIntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    double[] intersectionPoint = new double[2];
+    intersectionPoint[0] = Math.Round((b1-b2)/(k2-k1), 2);
+    intersectionPoint[1] = Math.Round(k1 * intersectionPoint[0] + b1, 2);
+    return intersectionPoint;
+}
+
+double b1 = getUserNumber("Введите b1: ");
+double k1 = getUserNumber("Введите k1: ");
+double b2 = getUserNumber("Введите b2: ");
+double k2 = getUserNumber("Введите k2: ");
+double[] resultIntersectionPoint = getIntersectionPoint(b1, k1, b2, k2);
+Console.WriteLine($"Точка пересечения прямых = ({resultIntersectionPoint[0]}, {resultIntersectionPoint[1]})");
